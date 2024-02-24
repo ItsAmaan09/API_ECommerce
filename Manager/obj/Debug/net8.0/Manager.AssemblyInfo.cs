1cd5e4bfd1ec89afef399bb86174c8bb73ae8dba
Mohammed Amaan <amaanvisiontech@gmail.com>
2024-02-24 13:16:52 +0530

    commit dll

--- Manager/obj/Debug/net8.0/Manager.AssemblyInfo.cs
+++ Manager/obj/Debug/net8.0/Manager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbd70a867dbcee18f083080122e09a550cf880f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d0a1ec1e94249b84d9a326ffd470e376dbaef91")]
 [assembly: System.Reflection.AssemblyProductAttribute("Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SQLRepository/obj/Debug/net8.0/SQLRepository.AssemblyInfo.cs
+++ SQLRepository/obj/Debug/net8.0/SQLRepository.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SQLRepository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbd70a867dbcee18f083080122e09a550cf880f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d0a1ec1e94249b84d9a326ffd470e376dbaef91")]
 [assembly: System.Reflection.AssemblyProductAttribute("SQLRepository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SQLRepository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
