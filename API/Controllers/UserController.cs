305f6334554246ce419a2ac999ca6de2782fd2c0
Mohammed Amaan <amaanvisiontech@gmail.com>
2024-02-26 21:50:14 +0530

    commit to add refe

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Manager;
+using Models;
 
 namespace API.Controllers
 {
@@ -20,6 +21,14 @@ namespace API.Controllers
         {
             this.userManager = new UserManager();
             return Ok(this.userManager.GetUsers());
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult Add(User user)
+        {
+            this.userManager = new UserManager();
+            return Ok(this.userManager.AddUser(user));
         }
     }
 }
